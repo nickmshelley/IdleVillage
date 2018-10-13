bc4cfacf575c32e9f74cf95a9ebb03b69553ca9c
Nick Shelley <nickmshelley@gmail.com>
2018-10-13 13:27:58 -0600

    Initial Commit

---
+++ IdleVillage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IdleVillage
+//
+//  Created by Heather Shelley on 10/13/18.
+//  Copyright © 2018 Mine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ IdleVillageTests/IdleVillageTests.swift
@@ -0,0 +1,34 @@
+//
+//  IdleVillageTests.swift
+//  IdleVillageTests
+//
+//  Created by Heather Shelley on 10/13/18.
+//  Copyright © 2018 Mine. All rights reserved.
+//
+
+import XCTest
+@testable import IdleVillage
+
+class IdleVillageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
